a501a0691418e48e28fbc6bb12859794b4a97114
Leandro Coelho <76078663+WebTopera@users.noreply.github.com>
2024-10-29 21:35:47 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -36,5 +36,18 @@ int main()
                 printf("Invalido\n");
             }
     }
+    else if (opcao == 3){
+        Nome nome;
+        string n
+        printf("Se atente ao número máximo de 30 letras.\n")
+        printf("Digite o nome:")
+        getline(cin,n);
+        if (nome.setName(n)) { // Tenta definir o nome
+            printf("Nome definido: %s\n", nome.getName().c_str()); // Exibe o nome definido
+        } 
+        else {
+            printf("Nome inválido! O nome deve ter entre 1 e 30 caracteres.\n");
+            }
+        }
     return 0;
 }
