235179f311f4efcd9b7159bb28506fa086b13c5a
Leandro Coelho <76078663+WebTopera@users.noreply.github.com>
2024-11-08 20:42:05 -0300

    Update main.cpp
    Adição na interface de Data, Avaliação e Duração.

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main()
     if(opcao == 1){
             Horario horario;
             string hora;
-            printf("Digite o horário: ");
+            printf("Digite o horario: ");
             getline(cin, hora);
             if(horario.setHora(hora)){
                 printf("Valor = %s\n", horario.getHora().c_str());
@@ -38,16 +38,53 @@ int main()
     }
     else if (opcao == 3){
         Nome nome;
-        string n
-        printf("Se atente ao número máximo de 30 letras.\n")
-        printf("Digite o nome:")
-        getline(cin,n);
-        if (nome.setName(n)) { // Tenta definir o nome
+        string nome_valor;
+        printf("Se atente ao numero maximo de 30 letras.\n");
+        printf("Digite o nome:");
+        getline(cin,nome_valor);
+        if (nome.setName(nome_valor)) { // Tenta definir o nome
             printf("Nome definido: %s\n", nome.getName().c_str()); // Exibe o nome definido
         } 
         else {
-            printf("Nome inválido! O nome deve ter entre 1 e 30 caracteres.\n");
+            printf("Nome invalido! O nome deve ter entre 1 e 30 caracteres.\n");
             }
         }
+     else if (opcao == 4){
+          Duracao duracao;
+          int duracao_valor;
+          printf("Digite a duracao: ");
+          cin >> duracao_valor;
+          if(duracao.setDuracao(duracao_valor)){
+               printf("Duracao = %s\n", duracao.getDuracao().c_str());
+          }
+          else {
+               printf("Invalido\n");
+          }
+
+     }
+     else if (opcao == 5){
+          Avaliacao avaliacao;
+          int avaliacao_valor;
+          printf("Digite a avaliacao: ");
+          cin >> avaliacao_valor;
+          if(avaliacao.setAvaliacao(avaliacao_valor)){
+               printf("Avaliacao = %s\n", avaliacao.getAvaliacao().c_str());
+          }
+          else {
+               printf("Invalido\n");
+          }
+     }
+     else if (opcao == 6){
+         Data data;
+         string data_valor;
+         printf("Digite a data: ");
+         cin >> data_valor;
+         if(data.setData(data_valor)){
+               printf("Data = %s\n", data.getData().c_str());
+          }
+          else {
+               printf("Invalido\n");
+          }
+     }
     return 0;
 }
