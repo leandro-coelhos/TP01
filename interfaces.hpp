5d5741af7dfcf0578edeb4c0e41686901bbe9bdf
Leandro Coelho <leandrocs1500@gmail.com>
2025-01-30 20:37:12 -0300

    Interfaces

---
+++ interfaces.hpp
@@ -0,0 +1,11 @@
+#ifndef INTERFACES_H_INCLUDED
+#define INTERFACES_H_INCLUDED
+
+#include "dominios.hpp"
+#include "entidades.hpp"
+
+class IServicoAutenticacao;
+class IServicoConta;
+class IServicoViagem;
+
+#endif // INTERFACES_H_INCLUDED
